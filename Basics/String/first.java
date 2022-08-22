995e9d0039a8dc7fe2f6caaaae4804e1239b44d7
Humam <104218563+humamul@users.noreply.github.com>
2022-08-22 19:08:09 +0530

    Update first.java

--- Basics/String/first.java
+++ Basics/String/first.java
@@ -16,3 +16,42 @@ Another Question
    }
    return count; // your code here        
     }
+
+
+// you can convert Whole String str to upper case;
+Str.toUpperCase();
+
+
+
+
+
+Q3
+// Input:
+// S = "#GeeKs01fOr@gEEks07"
+// Output:
+// 5
+// 8
+// 4
+// 2
+// Explanation: There are 5 uppercase characters,
+// 8 lowercase characters, 4 numeric characters
+// and 2 special characters.
+
+
+class Sol
+{
+    int[] count (String s)
+    {
+       int l=0,u=0,sc=0,n=0;
+       
+        for(int i=0;i<s.length();i++){
+            if(Character.isUpperCase(s.charAt(i))) u++;
+            else if(Character.isLowerCase(s.charAt(i))) l++;
+            else if(Character.isDigit(s.charAt(i))) n++;
+            else sc++;
+        }
+        
+        int[] sa= {u,l,n,sc};
+        return sa;
+    }
+}
