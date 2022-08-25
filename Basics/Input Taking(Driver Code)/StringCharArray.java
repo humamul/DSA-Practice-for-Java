42c3bd3cd35221909fe4427f17c55aa05c0531c0
Humam <104218563+humamul@users.noreply.github.com>
2022-08-25 21:33:12 +0530

    Update and rename Read.txt to StringCharArray.java

---
+++ Basics/Input Taking(Driver Code)/StringCharArray.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+import java.util.stream.*;
+import java.util.stream.Collectors.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+     String s1 = br.readLine();
+     String s2 = br.readLine();
+     s2 = s2.replaceAll(" ","");
+     s1 = s1.replaceAll(" ","");
+    char[] c2 = s2.toCharArray();
+    char[] c1 = s1.toCharArray();
+    Arrays.sort(c2); Arrays.sort(c1);
+    if(String.valueOf(c2).equals(String.valueOf(c1))){
+        System.out.println("True");
+    }else{
+        System.out.println("False");
+    }
+// System.out.print(String.valueOf(c1));
+ 
+    }
+    
+}//Enter code here
