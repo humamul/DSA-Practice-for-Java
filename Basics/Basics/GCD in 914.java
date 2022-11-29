31a3254e6e7eb696d4adb4f60b3ed1dca4e2ee6e
Humam Ul Islam <104218563+humamul@users.noreply.github.com>
2022-11-29 23:28:55 +0530

    Create GCD in 914.java

---
+++ Basics/Basics/GCD in 914.java
@@ -0,0 +1,21 @@
+   HashMap<Integer,Integer> hm = new HashMap<>(); /////  LC: 914
+        
+        int n =arr.length;
+        for(int i=0;i<n;i++){
+            hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+        }
+        
+        int ans = 0;
+        for(Integer i:hm.keySet()){
+            ans  = gcd (ans,hm.get(i));
+        }
+        System.out.println(hm+" "+ans);
+        if(ans>1) return true;
+        return false;
+    }
+    
+    static int gcd(int a,int b){
+        if(b==0) return a;
+        
+        return gcd(b,a%b);
+    }
